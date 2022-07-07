18ed7fddb5e0125723e6c64365cc3aa4d41dba70
dorian2788 <doris.tran09@protonmail.com>
2022-07-07 22:35:25 +1000

    lua revolution

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,97 @@
+local keymap = vim.keymap.set
+
+local opts = {silent = true}
+
+--leader
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+
+-- Y behave like captial letters
+keymap("n", "Y", "y$", opts)
+
+-- Centered
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
+keymap("n", "J", "mxJ`z", opts)
+
+-- Better window navigation
+keymap("n", "<leader>v", ":vsplit<CR>", opts)
+keymap("n", "<leader>h", ":wincmd h<CR>", opts)
+keymap("n", "<leader>j", ":wincmd j<CR>", opts)
+keymap("n", "<leader>k", ":wincmd k<CR>", opts)
+keymap("n", "<leader>l", ":wincmd l<CR>", opts)
+
+-- Clear highlights
+keymap("n", "<C-h>", "<cmd>nohlsearch<CR>", opts)
+
+--Close buffers
+keymap("n", "<leader>b", ":w<bar>%bd<bar>e#<CR>", opts)
+
+-- cpy clipboard
+keymap("n", "<leader>y", '"+y', opts)
+
+-- cpy all
+keymap("n", "<leader>Y", 'gg"+yG', opts)
+
+-- Insert --
+
+-- undo break points
+keymap("i", ",", ",<C-g>u", opts)
+keymap("i", ".", ".<C-g>u", opts)
+keymap("i", "!", "!<C-g>u", opts)
+keymap("i", "?", "?<C-g>u", opts)
+
+-- {
+-- }
+keymap("i", "{<CR>", "{}<esc>i<CR><esc>O", opts)
+
+-- /**
+keymap("i", "/**<CR>", "/**<CR><CR><BS>/<esc>i*<esc>ko", opts)
+
+-- Visual --
+
+-- Move blocks of text
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- go down one line
+keymap("v", "<leader>p", "_dP", opts)
+
+-- cpy clipboard
+keymap("v", "<leader>y", '"+y', opts)
+
+-- Telescope --
+keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
+keymap("n", "<leader>ft", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+keymap("n", "<leader>fp", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
+keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+
+-- TreeSitter --
+keymap("n", "<F7>", "<cmd>TSHighlightCapturesUnderCursor<cr>", opts)
+keymap("n", "<F8>", "<cmd>TSPlaygroundToggle<cr>", opts)
+
+-- Nvim-Tree --
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+-- Format --
+keymap("n", "<leader>f", ":Format<cr>", opts)
+
+-- Git --
+keymap("n", "<leader>ga", ":Git fetch --all<CR>")
+keymap("n", "<leader>grum", ":Git rebase upstream/master<CR>")
+keymap("n", "<leader>grom", ":Git rebase origin/master<CR>")
+
+keymap("n", "<leader>gh", ":diffget //3<CR>")
+keymap("n", "<leader>gf", ":diffget //2<CR>")
+keymap("n", "<leader>gs", ":G<CR>")
+keymap("n", "<leader>gd", ":Git difftool<CR>")
---
+++ lua/user/lualine.lua
@@ -0,0 +1,192 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  return
+end
+
+vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#d65d0e", bg = "#504945"})
+vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#fbf1c7", bg = "#504945", bold = false })
+vim.api.nvim_set_hl(0, "SLProgress", { fg = "#fbf1c7", bg = "#665c54" })
+vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#d5c4a1", bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "SLLSP", { fg = "#fabd2f", bg = "#3c3836" })
+
+local mode_color = {
+  n = "#b8bb26",
+  i = "#83a598",
+  v = "#d65d0e",
+  [""] = "#d3869b",
+  V = "#b16286",
+  c = "#83a598",
+  o = "#61afef",
+  s = "#d19a66",
+  S = "#d19a66",
+  [""] = "#d19a66",
+  ic = "#e06c75",
+  R = "#d79921",
+  Rv = "#e06c75",
+  cv = "#61afef",
+  ce = "#61afef",
+  r = "#e06c75",
+  rm = "#56b6c2",
+  ["r?"] = "#56b6c2",
+  ["!"] = "#56b6c2",
+  t = "#98c379",
+}
+
+local mode = {
+  -- mode component
+  function()
+    return "  "
+  end,
+  color = function()
+    -- auto change color according to neovims mode
+    return { bg = mode_color[vim.fn.mode()] }
+  end,
+  -- padding = { right = 1 },
+  padding = 0,
+}
+
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+local icons = require "user.icons"
+
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = icons.diagnostics.Error .. " ", warn = icons.diagnostics.Warning .. " " },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
+}
+
+local diff = {
+  "diff",
+  colored = false,
+  symbols = { added = icons.git.Add .. " ", modified = icons.git.Mod .. " ", removed = icons.git.Remove .. " " }, -- changes diff symbols
+  cond = hide_in_width,
+  separator = "%#SLSeparator#" .. "│ " .. "%*",
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = true,
+  -- icon = nil,
+}
+
+local branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "%#SLGitIcon#" .. "" .. "%*" .. "%#SLBranchName#",
+  -- color = "Constant",
+  colored = false,
+}
+
+local progress = {
+  "progress",
+  color = "SLProgress",
+}
+
+local current_signature = function()
+  if not pcall(require, "lsp_signature") then
+    return
+  end
+  local sig = require("lsp_signature").status_line(30)
+  return "%#SLSeparator#" .. sig.hint .. "%*"
+end
+
+local spaces = {
+  function()
+    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  end,
+  padding = 0,
+  separator = "%#SLSeparator#" .. " │" .. "%*",
+}
+
+local lanuage_server = {
+  function()
+    local clients = vim.lsp.buf_get_clients()
+    local client_names = {}
+
+    -- add client
+    for _, client in pairs(clients) do
+      if client.name ~= "copilot" and client.name ~= "null-ls" then
+        table.insert(client_names, client.name)
+      end
+    end
+
+    local buf_ft = vim.bo.filetype
+
+    -- add formatter
+    local s = require "null-ls.sources"
+    local available_sources = s.get_available(buf_ft)
+    local registered = {}
+    for _, source in ipairs(available_sources) do
+      for method in pairs(source.methods) do
+        registered[method] = registered[method] or {}
+        table.insert(registered[method], source.name)
+      end
+    end
+
+    local formatter = registered["NULL_LS_FORMATTING"]
+    local linter = registered["NULL_LS_DIAGNOSTICS"]
+    if formatter ~= nil then
+      vim.list_extend(client_names, formatter)
+    end
+    if linter ~= nil then
+      vim.list_extend(client_names, linter)
+    end
+
+    -- join client names with commas
+    local client_names_str = table.concat(client_names, ", ")
+    -- 
+    return "%#SLLSP#" .. "[" .. client_names_str .. "]" .. "%*"
+  end,
+  padding = 0,
+  cond = hide_in_width,
+  separator = "%#SLSeparator#" .. " │ " .. "%*",
+}
+
+local location = {
+  "location",
+  color = function()
+    -- darkerplus
+    -- return { fg = "#252525", bg = mode_color[vim.fn.mode()] }
+    return { fg = "#1E232A", bg = mode_color[vim.fn.mode()] }
+  end,
+}
+
+lualine.setup {
+  options = {
+    globalstatus = true,
+    icons_enabled = true,
+    theme = "gruvbox_dark",
+    -- theme = theme,
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard" },
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { mode, branch },
+    lualine_b = { diagnostics },
+    -- lualine_c = {},
+    lualine_c = { { current_signature, cond = hide_in_width } },
+    -- lualine_x = { diff, spaces, "encoding", filetype },
+    lualine_x = { diff, lanuage_server, spaces, filetype },
+    lualine_y = { progress },
+    lualine_z = { location },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {},
+}
+
